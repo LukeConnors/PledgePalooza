d4a95779ebdc4710c84a6256b438593865dd7f73
Tristanleif1 <tristanallaman@gmail.com>
2023-08-27 07:54:38 -0700

    Added all models to __init__ file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,8 @@ from .db import db
 from .user import User
 from .db import environment, SCHEMA
 from .project import Project
-
+from .backed_project import BackedProject
+from .category import Category
+from .image import Image
+from .reward import Reward
 
