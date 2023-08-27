36abe65288c6294d466aca4e281861d10fbfc690
Tristanleif1 <tristanallaman@gmail.com>
2023-08-27 08:41:04 -0700

    Adjusted add prefix helper function location

--- app/models/backed_project.py
+++ app/models/backed_project.py
@@ -8,9 +8,9 @@ class BackedProject(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    projectId = db.Column(db.Integer, add_prefix_for_prod(db.ForeignKey('projects.id')), nullable=False)
-    rewardId = db.Column(db.Integer, add_prefix_for_prod(db.ForeignKey('rewards.id')), nullable=False)
-    userId = db.Column(db.Integer, add_prefix_for_prod(db.ForeignKey('users.id')), nullable=False)
+    projectId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('projects.id')), nullable=False)
+    rewardId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('rewards.id')), nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     cost = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=func.now())
     updated_at = db.Column(db.DateTime, nullable=False, default=func.now(), onupdate=func.now())
