e8a93b4d7c65b387fcce6745b480bf8efeda8688
Tristanleif1 <tristanallaman@gmail.com>
2023-08-27 07:42:18 -0700

    added prefix_for_prod helper function to backedProjects model

--- app/models/backed_project.py
+++ app/models/backed_project.py
@@ -2,15 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, DateTime, func
 from datetime import datetime
 from user import User
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class BackedProject(db.Model):
     __tablename__ = 'backed_projects'
-    
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    projectId = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable=False)
-    rewardId = db.Column(db.Integer, db.ForeignKey('rewards.id'), nullable=False)
-    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    projectId = db.Column(db.Integer, add_prefix_for_prod(db.ForeignKey('projects.id')), nullable=False)
+    rewardId = db.Column(db.Integer, add_prefix_for_prod(db.ForeignKey('rewards.id')), nullable=False)
+    userId = db.Column(db.Integer, add_prefix_for_prod(db.ForeignKey('users.id')), nullable=False)
     cost = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=func.now())
     updated_at = db.Column(db.DateTime, nullable=False, default=func.now(), onupdate=func.now())
