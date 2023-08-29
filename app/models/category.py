1bb9c8f10340bbba757e9c9189c93a064b5aae92
LukeConnors <LukeTConnors@yahoo.com>
2023-08-28 20:35:05 -0500

    added if conditional for category model

--- app/models/category.py
+++ app/models/category.py
@@ -5,11 +5,13 @@ from .db import db
 class Category(db.Model):
     __tablename__ = 'categories'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
 
-    
+
     # INTERNAL-MODEL RELATIONS(PRIMARY-KEY):
     project = db.relationship('Project', back_populates='category')
-
-    
