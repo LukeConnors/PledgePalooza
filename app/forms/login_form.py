eb2ed7ee3338c7f2fc28d9970e9019b853daa9fa
Albert Marrero <albert.marrero10@gmail.com>
2023-09-09 16:54:08 -0400

    Fixed log in bug

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -6,21 +6,20 @@ from app.models import User
 def user_exists(form, field):
 
     email = field.data
-    user = User.query.filter(User.email.first())
+    user = User.query.filter_by(email=email).first()
     if not user:
         raise ValidationError('Email provided not found.')
 
 def password_matches(form, field):
 
     password = field.data
-    email = form.data['email']
+    email = form.email.data
 
-
-    user = User.query.filter(User.email.ilike(email)).first()
+    user = User.query.filter_by(email=email).first()
 
     if user and not user.check_password(password):
         raise ValidationError('Password was incorrect.')
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired(), password_matches])
+    email = StringField('Email', validators=[DataRequired(), user_exists])
+    password = StringField('Password', validators=[DataRequired(), password_matches])
