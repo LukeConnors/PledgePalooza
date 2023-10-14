d3905d107a837d0723c2d7d9f6fe4953d8747566
Tristanleif1 <tristanallaman@gmail.com>
2023-10-14 14:57:49 -0700

    MyLikes page fully functional

--- react-app/src/components/MyLikedProjects/index.js
+++ react-app/src/components/MyLikedProjects/index.js
@@ -2,24 +2,32 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import { loadMyLikes } from "../../store/likes";
-import "./MyLikedProjects.css"
+import { deleteLike } from "../../store/likes";
+import "./MyLikedProjects.css";
+import thumbsUp from "../../assets/thumbs-up-icon.png"
 
 function MyLikedProjects() {
     const [likedProjects, setLikedProjects] = useState([]);
     const history = useHistory()
     const user = useSelector((state) => state.session.user)
     const likes =  useSelector(state => Object.values(state.like))
-    console.log(likes);
     const dispatch = useDispatch()
     if(!user){
         history.push("/");
     }
 
+    const handleLikeClick = async (projectId, e) => {
+        e.preventDefault();
+        console.log('projectId:', projectId)
+          dispatch(deleteLike(null, projectId));  
+          setLikedProjects(likedProjects?.filter(id => id !== projectId));
+        }
+
     useEffect(() => {
         dispatch(loadMyLikes()).then(likedProjectsDetails => {
             setLikedProjects(likedProjectsDetails || []);
         })
-    }, [user, dispatch]);
+    }, [user, dispatch, likedProjects]);
     console.log(likedProjects);
 
 
@@ -37,6 +45,12 @@ function MyLikedProjects() {
                         alt={`${project.name}`}
                         src={project.bannerImg}
                       ></img>
+                      <img
+                        className="my-like-logo" 
+                          src={thumbsUp}
+                          alt="thumbs up icon"
+                          onClick={(e) => handleLikeClick(project.id, e)}
+                        />
                       <div className="my-liked-name-holder">
                         <h2 className="my-liked-project-name" key={project.id}>
                           {project.name}
