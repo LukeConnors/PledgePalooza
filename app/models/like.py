c033714c1b308ca4a9a38152989569f46fe0b919
LukeConnors <LukeTConnors@yahoo.com>
2023-10-22 03:33:02 -0500

    test

--- app/models/like.py
+++ app/models/like.py
@@ -4,7 +4,11 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Like(db.Model):
     __tablename__ = 'likes'
-    
+
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     projectId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('projects.id')), nullable=False)
@@ -13,12 +17,12 @@ class Like(db.Model):
     project = db.relationship('Project', back_populates='like')
     user = db.relationship('User', back_populates='like')
 
-    if environment == "production":
-        __table_args__ = (UniqueConstraint(userId, projectId, name='unique_liked_project'), {'schema': SCHEMA})
-    else:
-        __table_args__ = (UniqueConstraint(userId, projectId, name='unique_liked_project'),)
+    # if environment == "production":
+    #     __table_args__ = (UniqueConstraint(userId, projectId, name='unique_liked_project'), {'schema': SCHEMA})
+    # else:
+    #     __table_args__ = (UniqueConstraint(userId, projectId, name='unique_liked_project'),)
 
-    
+
     def to_dict(self):
         return {
             'id': self.id,
