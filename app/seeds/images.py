5fd4afd2ff959cd479c8446fa67197c3b6d6ca6f
Tristanleif1 <tristanallaman@gmail.com>
2024-09-24 21:52:01 -0700

    Replaced all projects to point to new AWS S3 Bucket

--- app/seeds/images.py
+++ app/seeds/images.py
@@ -9,69 +9,81 @@ def seed_images():
     )
 
     project_image2 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/Cosmic2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/Cosmic_Odyssey_pic2.png",
         imageable_id=3,
         imageable_type="project"
     )
 
     project_image3 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/Arcane2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/Arcane_Alchemy_pic2.png",
         imageable_id=4,
         imageable_type="project"
     )
     project_image4 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/DragonsDomain2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/DragonsDominion_pic2.png",
         imageable_id=6,
         imageable_type="project"
     )
     project_image5 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/aquaventure2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/UnderwaterVROydssey_pic2.png",
         imageable_id=7,
         imageable_type="project"
     )
     project_image6 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/quantumx2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/QuantumX_pic2.png",
         imageable_id=8,
         imageable_type="project"
     )
     project_image7 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/lunalens2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/LunaLens_pic2.png",
         imageable_id=9,
         imageable_type="project"
     )
     project_image8 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/skyhawk2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/SkyHawk_pic2.png",
         imageable_id=10,
         imageable_type="project"
     )
     project_image9 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/ecostyle2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/EcoStyle_pic2.png",
         imageable_id=12,
         imageable_type="project"
     )
     project_image10 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/urbanthreads2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/UrbanThreads_pic2.png",
         imageable_id=13,
         imageable_type="project"
     )
     project_image11 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/epictales2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/EpicTales_pic2.png",
         imageable_id=14,
         imageable_type="project"
     )
     project_image12 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/epictales3.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/EpicTales_pic1.png",
         imageable_id=14,
         imageable_type="project"
     )
     project_image13 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/cookbook2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/CookbookChronicles_pic2.png",
         imageable_id=15,
         imageable_type="project"
     )
     project_image14 = Image(
-        url= "https://pledgepaloozabucket.s3.us-east-2.amazonaws.com/mystery2.png",
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/Whodunit_Mysteries_pic2.png",
         imageable_id=16,
+        imageable_type="project"
+    )
+
+    project_image15 = Image(
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/Cybernetica_pic2.png",
+        imageable_id=5,
+        imageable_type="project"
+    )
+
+    project_image16 = Image(
+        url= "https://pledgepalooza-images.s3.us-east-2.amazonaws.com/EcoCharge_pic2.png",
+        imageable_id=11,
         imageable_type="project"
     )
 
@@ -101,6 +113,8 @@ def seed_images():
     db.session.add(project_image12)
     db.session.add(project_image13)
     db.session.add(project_image14)
+    db.session.add(project_image15)
+    db.session.add(project_image16)
     db.session.add(reward_image1)
 
 
